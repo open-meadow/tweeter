8562300190a06ac4c4673d549c717d3f2ec618d8
open-meadow <rohan.varughese@protonmail.com>
2022-12-07 10:29:05 -0500

    Implement dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,20 +9,33 @@ $(document).ready(function () {
 
   
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   
   const createTweetElement = function(tweetData) {
-    console.log(tweetData.content.text);
+    // create tweet element
     const tweet = $(`
     <div class="tweet-container">
       <header>
@@ -44,12 +57,21 @@ $(document).ready(function () {
     return tweet;
   }
 
-  const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // get return value and append it to tweets container
 
-  const renderTweets = function() {}
+    tweets.forEach(element => {
+      const $tweet = createTweetElement(element);
+      $("#tweets-container").append($tweet[0]);
+    });
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet[0]); // to see what it looks like
-  $("#tweets-container").append($tweet[0]); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet[0]); // to see what it looks like
+  // $("#tweets-container").append($tweet[0]); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
