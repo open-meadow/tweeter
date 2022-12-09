2f13f7fc3bb67e7ad2363236f7665d66b86682c4
open-meadow <rohan.varughese@protonmail.com>
2022-12-09 18:38:36 -0500

    Refactor char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,41 +1,45 @@
 $(document).ready(function () {
-
-  $(".input-bar").on("keyup", function () {
-    const $inputElement = $(this);
-    const divButtonsElement = $inputElement.next()[0];
-    const $divButtonsElement = $(divButtonsElement);
-    const counterElement = $divButtonsElement.find("p")[0]; 
-
-
-    const counter = $(counterElement);
-    const characterCount = 140 - $(this).val().length;
-
-    // display in HTML
-    counter.text(characterCount);
-
-    if (characterCount < 0) {
-      counter.addClass("red");
-    } else {
-      counter.removeClass("red");
-    }
-  });
-
-  // this section scrolls to reveal scroll-up button
-  $(window).scroll(function() {
-    if($(window).scrollTop() > 300) {
-      // stop() prevents blinking
-      $("nav").stop().fadeOut();
-      $("#go-up-button").stop().fadeIn();
-    } else {
-      $("nav").stop().fadeIn();
-      $("#go-up-button").stop().fadeOut();
-    }
-  });
-
-  // this section makes the window go up when the button is pressed
-  $("#go-up-button").click(function() {
-    $("html, body").animate({scrollTop:0}, 300);
-    $(".new-tweet").slideDown();
-  });
-
+  $(".input-bar").on("input", onInput);
+  $(window).scroll(scrollWindow);
+  $("#go-up-button").click(goUp);
 });
+
+const onInput = function (event) {
+  const $inputElement = $(this);
+  const divButtonsElement = $inputElement.next()[0];
+  const $divButtonsElement = $(divButtonsElement);
+  const counterElement = $divButtonsElement.find(".counter")[0];
+
+  const counter = $(counterElement);
+
+  // number of characters user has input
+  let len = $(this).val().length;
+  const characterCount = 140 - len;
+
+  // display in HTML
+  counter.text(characterCount);
+
+  if (characterCount < 0) {
+    counter.addClass("red");
+  } else {
+    counter.removeClass("red");
+  }
+};
+
+// this section scrolls to reveal scroll-up button
+const scrollWindow = () => {
+  if ($(window).scrollTop() > 300) {
+    // stop() prevents blinking
+    $("nav").stop().fadeOut();
+    $("#go-up-button").stop().fadeIn();
+  } else {
+    $("nav").stop().fadeIn();
+    $("#go-up-button").stop().fadeOut();
+  }
+};
+
+// this section makes the window go up when the button is pressed
+const goUp = (event) => {
+  $("html, body").animate({ scrollTop: 0 }, 300);
+  $(".new-tweet").slideDown();
+};
